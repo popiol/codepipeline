9fc53d014d774fe600bca12dd058827dce4304d9
popiol <opiola.piotr@gmail.com>
2020-02-23 12:39:33 +0100

    buildspec remove sudo

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,6 +10,13 @@ resource "aws_subnet" "subnet1" {
 	tags = var.tags
 }
 
+resource "aws_subnet" "subnet2" {
+	vpc_id = aws_vpc.vpc1.id
+	cidr_block = "172.16.20.0/24"
+	availability_zone = "${data.aws_region.current.name}b"
+	tags = var.tags
+}
+
 resource "aws_security_group" "sec_gr1" {
 	name = "${var.app_id}_sec_gr1"
 	vpc_id = aws_vpc.vpc1.id
@@ -43,6 +50,12 @@ resource "aws_internet_gateway" "gateway1" {
 	tags = var.tags
 }
 
+resource "aws_nat_gateway" "nat1" {
+	allocation_id = aws_eip.eip2.id
+	subnet_id = aws_subnet.subnet1.id
+	tags = var.tags
+}
+
 resource "aws_route_table" "route1" {
 	vpc_id = aws_vpc.vpc1.id
 	tags = var.tags
@@ -53,14 +66,27 @@ resource "aws_route_table" "route1" {
 	}
 }
 
+resource "aws_route_table" "route2" {
+	vpc_id = aws_vpc.vpc1.id
+	tags = var.tags
+
+    route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_nat_gateway.nat1.id
+	}
+}
+
 resource "aws_route_table_association" "route1_subnet1" {
 	subnet_id = aws_subnet.subnet1.id
 	route_table_id = aws_route_table.route1.id
 }
 
-#resource "aws_nat_gateway" "nat1" {
-#	allocation_id = aws_eip.eip1.id
-#	subnet_id = aws_subnet.subnet1.id
-#	tags = var.tags
-#}
+resource "aws_route_table_association" "route2_subnet2" {
+	subnet_id = aws_subnet.subnet2.id
+	route_table_id = aws_route_table.route2.id
+}
+
+resource "aws_eip" "eip2" {
+	vpc = true
+}
 
