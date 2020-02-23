88b03bdecd72c83b99522e6ed419c65d38660bc4
popiol <opiola.piotr@gmail.com>
2020-02-23 21:12:02 +0100

    buildspec submodule

--- config/kube_init.sh
+++ config/kube_init.sh
@@ -2,25 +2,31 @@
 
 #set timezone
 
-sudo echo "$timezone" > /etc/timezone
+sudo echo "${timezone}" > /etc/timezone
 
 #copy vim config file
 
-sudo cat "$vimrc" > /etc/vim/vimrc.local
+sudo echo "${vimrc}" > /etc/vim/vimrc.local
+
+#copy docker-compose.yml
+
+cd /tmp
+
+echo "${docker_compose}" > ./docker-compose.yml
 
 #install kubectl
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+#curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 
-chmod +x ./kubectl
+#chmod +x ./kubectl
 
-sudo mv ./kubectl /usr/local/bin/kubectl
+#sudo mv ./kubectl /usr/local/bin/kubectl
 
 #install docker
 
 sudo apt-get update 
 
-sudo apt-get install docker.io -y
+sudo apt-get install -y docker.io
 
 #install docker compose
 
@@ -30,13 +36,23 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 #install minikube
 	
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
+#curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
 
-chmod +x minikube 
+#chmod +x minikube 
 
-sudo mv minikube /usr/local/bin/
+#sudo mv minikube /usr/local/bin/
 
 #start minikube
 
-sudo minikube start --vm-driver=none
+#sudo minikube start --vm-driver=none
+
+#install AWS CLI
+
+sudo apt-get install -y awscli
+
+#login to AWS ECR
+
+sudo $(aws ecr get-login --no-include-email --region ${aws_region})
+
+sudo docker-compose up -d
 
